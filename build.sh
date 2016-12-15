36e3db1402858a1ec331c9c3ed4dd405ff1a6396
Jussi Räsänen <jussi.rasanen@leonidasoy.fi>
2016-12-15 09:53:18 +0200

    Version bump, allowed udp

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 cd elasticsearch/
-docker build -t image-registry.leonidasoy.fi/loki-elasticsearch:v3 .
-docker push image-registry.leonidasoy.fi/loki-elasticsearch:v3
+docker build -t image-registry.leonidasoy.fi/loki-elasticsearch:v4 .
+docker push image-registry.leonidasoy.fi/loki-elasticsearch:v4
 cd ..
 
 cd fluentd/
-docker build -t image-registry.leonidasoy.fi/loki-fluentd:v3 .
-docker push image-registry.leonidasoy.fi/loki-fluentd:v3
+docker build -t image-registry.leonidasoy.fi/loki-fluentd:v4 .
+docker push image-registry.leonidasoy.fi/loki-fluentd:v4
 cd ..
 
 cd kibana/
-docker build -t image-registry.leonidasoy.fi/loki-kibana:v3 .
-docker push image-registry.leonidasoy.fi/loki-kibana:v3
+docker build -t image-registry.leonidasoy.fi/loki-kibana:v4 .
+docker push image-registry.leonidasoy.fi/loki-kibana:v4
 cd ..
